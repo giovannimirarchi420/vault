db6d5f5df89172bbebc08d5059f17af41421b81f
Giovanni Mirarchi <giovanni.mirarchi@hotmail.com>
2022-04-03 14:59:52 +0200

    refact: add VaultConfig class, write and read now performed with VaultOperations object

--- src/main/kotlin/it/polito/vault/VaultProject/controllers/Controller.kt
+++ src/main/kotlin/it/polito/vault/VaultProject/controllers/Controller.kt
@@ -6,8 +6,6 @@ import it.polito.vault.VaultProject.services.VaultService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.lang.Exception
-import java.util.*
 
 @RestController
 class Controller(var service: VaultService) {
@@ -16,23 +14,15 @@ class Controller(var service: VaultService) {
     fun addSecret(@PathVariable secretsName: String,
                   @RequestBody secret: SecretDTO
     ) : ResponseEntity<BaseResponse> {
-        try {
-            var response = service.writeSecret(secretsName, secret)
-            if(Objects.isNull(response))
-                return ResponseEntity(BaseResponse(true, "And error occurred reading"), HttpStatus.INTERNAL_SERVER_ERROR)
-            return ResponseEntity(BaseResponse(false, response.toString()), HttpStatus.OK)
-        } catch (excepion: Exception) {
-            return ResponseEntity(BaseResponse(true, excepion.message.toString()), HttpStatus.BAD_REQUEST)
-        }
-        return ResponseEntity(BaseResponse(true, "ERROR"), HttpStatus.BAD_REQUEST)
+        val response = service.writeSecretWithOperationObject(secretsName, secret)
+        val status = if(response.error) HttpStatus.BAD_REQUEST else HttpStatus.OK
+        return ResponseEntity(response, status)
     }
 
     @GetMapping("/get/{pathName}")
     fun readSecret(@PathVariable pathName: String): ResponseEntity<BaseResponse>?{
-        var response = service.readSecret(pathName)
-        if(Objects.isNull(response)) {
-            return ResponseEntity<BaseResponse>(BaseResponse(true, "No secrets found for this path"), HttpStatus.BAD_REQUEST)
-        }
-        return ResponseEntity<BaseResponse>(BaseResponse(false, response), HttpStatus.OK)
+        val response = service.readSecretWithOperationObject(pathName)
+        val status = if(response.error) HttpStatus.BAD_REQUEST else HttpStatus.OK
+        return ResponseEntity(response, status)
     }
 }
--- src/main/kotlin/it/polito/vault/VaultProject/services/VaultService.kt
+++ src/main/kotlin/it/polito/vault/VaultProject/services/VaultService.kt
@@ -1,56 +1,50 @@
 package it.polito.vault.VaultProject.services
 
-import it.polito.vault.VaultProject.config.Config
+import it.polito.vault.VaultProject.dtos.BaseResponse
 import it.polito.vault.VaultProject.dtos.SecretDTO
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import org.springframework.vault.authentication.TokenAuthentication
-import org.springframework.vault.client.VaultEndpoint
-import org.springframework.vault.core.VaultTemplate
+import org.springframework.vault.core.VaultKeyValueOperationsSupport
+import org.springframework.vault.core.VaultOperations
 import java.util.*
 
 
 @Service
-class VaultService(val config: Config) {
+class VaultService(val vaultOperations: VaultOperations) {
 
     var logger: Logger = LoggerFactory.getLogger(VaultService::class.java)
 
-    fun writeSecret(name: String, secret: SecretDTO): Boolean {
-        val endpoint = VaultEndpoint.create("localhost", 8200)
+    fun writeSecretWithOperationObject(name: String, secret: SecretDTO) : BaseResponse {
         val path = "polito/$name"
-        endpoint.scheme = "http"
-        val template = VaultTemplate(endpoint, TokenAuthentication(config.token))
-
+        val keyValueOperations = vaultOperations.opsForKeyValue(path, VaultKeyValueOperationsSupport.KeyValueBackend.KV_1)
         try {
             logger.info("Attempting to write secret in vault...")
-            template.write(path, secret)
+            keyValueOperations.put(path, secret)
             logger.info("Secret written")
         } catch (e: Exception) {
             logger.error("Some error occurred writing a secret")
-            return false
+            return BaseResponse(true, "Error, secret not created")
         }
 
-        return true
+        return BaseResponse(false, "Secret created")
     }
 
-    fun readSecret(pathName: String): SecretDTO? {
-        val endpoint = VaultEndpoint.create("localhost", 8200)
+    fun readSecretWithOperationObject(pathName: String): BaseResponse {
         val path = "polito/$pathName"
-        endpoint.scheme = "http"
-        val template = VaultTemplate(endpoint, TokenAuthentication(config.token))
+        val keyValueOperations = vaultOperations.opsForKeyValue(path, VaultKeyValueOperationsSupport.KeyValueBackend.KV_1)
 
         try {
             logger.info("Attempting to read secret from vault...")
-            val secret = template.read(path, SecretDTO::class.java)?.data
+            val secret = keyValueOperations.get(path, SecretDTO::class.java)?.data
             if(Objects.isNull(secret)){
                 throw java.lang.Exception()
             }
             logger.info("Secret read")
-            return secret
+            return BaseResponse(false, secret)
         } catch (e: Exception) {
             logger.error("Some error occurred reading a secret")
         }
-        return null
+        return BaseResponse(true, "No secret found in $path")
     }
 }
